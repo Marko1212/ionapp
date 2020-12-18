16cc8c6f5eebda08c78cf06315ea918ab18852e0
Marko1212 <shihonage7@gmail.com>
2020-12-18 11:07:47 +0100

    settings

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,21 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
+
 export class SettingsPage implements OnInit {
 
-  settings = {pseudo: '', ville: '', darkmode: false};
+  settings = {username: '', city: '', darkMode: false};
 
-  constructor() { }
+  constructor(private storage: Storage) { }
 
   ngOnInit() {
+   
+    this.storage.get("settings").then(settings => this.settings = settings);
+   
   }
 
-  logForm(){
+  toggleDarkMode() {
+    document.body.classList.toggle('dark');
+  }
+
+  save(){
     console.log(this.settings);
+    //on va utiliser le service storage pour stocker 
+    //les settings sur le téléphone
+
+
+    this.storage.set("settings", this.settings);
+  }
+
+  removeData(){
+  
+    this.storage.remove("settings");
+    
+    this.settings = {username: '', city: '', darkMode: false};
+
   }
 
 }
