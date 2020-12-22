a00f752f457edadb33186d00384d76a6f548f01c
Marko1212 <shihonage7@gmail.com>
2020-12-22 15:19:31 +0100

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Camera} from '@ionic-native/camera/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  images = [];
 
-  constructor() {}
+  constructor(private camera: Camera,
+              private alert: AlertController) {}
+
+  takePicture() {
+    let options = {
+      destinationType: this.camera.DestinationType.DATA_URL
+    };
+    this.camera.getPicture(options).then(image => {
+      //on récupère la photo dans l'image
+      this.images.push('data:image/png;base64,' + image);
+      this.alert.create({
+        message: 'Salut ' + image
+      }).then(alert => alert.present());
+    });
+  }
 
 }
